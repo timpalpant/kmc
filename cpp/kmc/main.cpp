fddefb2d260367d4f6105a1eb19b881d788aa44e
Timothy Palpant <tim@palpant.us>
2013-03-28 22:24:56 -0400

    Merge C++ and Java versions

---
+++ cpp/kmc/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  kmc
+//
+//  Created by Timothy Palpant on 3/28/13.
+//  Copyright (c) 2013 Timothy Palpant. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+  // insert code here...
+  std::cout << "Hello, World!\n";
+    return 0;
+}
+
