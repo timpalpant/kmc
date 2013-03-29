c0adeaac5e84a09eb24cabdf7703186fc829cf6d
Timothy Palpant <tim@palpant.us>
2013-03-29 09:19:55 -0400

    Add main.cpp

--- cpp/kmc/main.cpp
+++ cpp/kmc/main.cpp
@@ -2,17 +2,20 @@
 //  main.cpp
 //  kmc
 //
-//  Created by Timothy Palpant on 3/28/13.
+//  Created by Timothy Palpant on 3/16/13.
 //  Copyright (c) 2013 Timothy Palpant. All rights reserved.
 //
 
 #include <iostream>
+#include "ark.h"
 
-int main(int argc, const char * argv[])
-{
-
-  // insert code here...
-  std::cout << "Hello, World!\n";
-    return 0;
+int main(int argc, const char * argv[]) {
+  if (argc < 2) {
+    std::cout << "USAGE: kmc [--include ARK] [--cfg KEY=VALUE]" << std::endl;
+    return 2;
+  }
+  
+  //config::Ark ark = config::Ark::fromArgv(argv);
+  ark::Ark ark;
 }
 
